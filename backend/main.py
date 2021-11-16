13d32d5458678f8b353ce12c79e50a54089079d1
a-chatterjee <achatterjee@planview.com>
2021-11-16 12:42:03 +0530

    checking in image to text api

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,8 @@
 from typing import Optional
 from pydantic import BaseModel
-
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+import shutil
+import easyocr
 
 app = FastAPI()
 
@@ -13,12 +14,27 @@ def read_root():
     return {"Key": "Something Interesting is cooking !"}
 
 @app.post("/image-to-text")
-def image_to_text(image: Image):
+def image_to_text(file: UploadFile = File(...)):
     ''' This handle takes the image in binary and then extracts the text out of it '''
-    return {"extracted_text": "An extracted text from the image", "title": f"{image.title}"}
+    file_location = f"files/{file.filename}"
+    with open(file_location, "wb+") as file_object:
+        shutil.copyfileobj(file.file, file_object)
+    text = imageToText(file_location)
+
+    return {"extracted_text": text}
 
 @app.get("/get-boards/{workspace-id}")
 def get_boards(workspace_id: int):
     ''' This handle takes workspace id and returns the list of boards'''
     return {"boards": []}
+
+
+def imageToText(image):
+    reader = easyocr.Reader(['en']) 
+    result = reader.readtext(image)
+    text = ''
+    for detection in result: 
+        text = text +" "+ detection[1]
+    return text
+
 
