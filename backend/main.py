669a57c7634b23df261a0cdefab8c62a1817aed5
a-chatterjee <achatterjee@planview.com>
2021-11-17 14:44:11 +0530

    api for posting card on pp board

--- backend/main.py
+++ backend/main.py
@@ -6,6 +6,8 @@ from spellchecker import SpellChecker
 import requests
 import json
 import configparser
+from pydantic import BaseModel
+from typing import Optional
 
 config = configparser.ConfigParser()
 config.read('settings.env')
@@ -15,6 +17,13 @@ app = FastAPI()
 reader = easyocr.Reader(['en'])
 spell = SpellChecker('en')
 CONFIDENCE_THRESHOLD = 80
+
+class Card(BaseModel):
+    projectId: int
+    boardId: int
+    title: str
+    description: Optional[str] = None
+
 
 @app.get("/")
 def read_root():
@@ -32,8 +41,14 @@ def image_to_text(file: UploadFile = File(...)):
 
 @app.get("/get-boards")
 def get_boards():
-    ''' This handle takes workspace id and returns the list of boards'''
+    ''' This handle returns the list of boards and their id and project id to which they belong'''
     return {"boards": [getBoards()]}
+
+@app.post("/post-ppCard")
+def post_projectPlace_card(card: Card):
+    ''' This handle takes a projectId, boardId and card title and description for new card and posts a projectPlace card'''
+    response  = postCard(card.projectId, card.boardId, card.title, card.description)
+    return response
 
 
 @timer_func
@@ -59,7 +74,24 @@ def getBoards():
     response = session.get('https://service.projectplace.com/api/v1/user/me/boards')
     boardList = []
 
+    #creates list of board and board-Id
     for boards in response.json():
-        boardList.append((boards.get('name'), boards.get('id')))  #creates list of board and board-Id
+        boardList.append((boards.get('name'), f"boardId = {boards.get('id')}", f"projectId = {boards.get('project_id')}"))
 
-    return boardList # sample output::: [["Generac Pursuit",1094435],["board3",1308404],["new board",1308397]]
+    return boardList 
+    # sample output::: [('Generac Pursuit', 'boardId = 1094435', 'projectId = 1127607142'), ('board3', 'boardId = 1308404', 'projectId = 1109821908')]
+
+
+def postCard(projectId, boardId, title, description):
+    header = {"content-type": "application/json"}
+    session = requests.Session()
+    session.headers.update({'Authorization': f"Bearer {token}"})
+    #creates new card with title
+    payload = {'board_id':boardId ,'column_id': 0, 'title': title}
+    responsePostCard = session.post(f'https://service.projectplace.com/api/v1/projects/{projectId}/cards/create-new' , data=json.dumps(payload), headers=header)
+    cardId = responsePostCard.json().get('id')
+
+    #updates description of card
+    responseUpdate = session.put(f'https://service.projectplace.com/api/v1/cards/{cardId}' , data=json.dumps({'description' : description}), headers=header)
+    return responseUpdate.json()
+
